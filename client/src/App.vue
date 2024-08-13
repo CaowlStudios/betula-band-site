5bafa40e6582f1a2857c60d573fe56eb1163eadb
CJ <iron768gamer@gmail.com>
2024-08-12 21:52:47 -0400

    Fix import

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@ import IntroPage from "./components/pages/IntroPage.vue";
 import MusicPage from "./components/pages/MusicPage.vue";
 import CalendarPage from "./components/pages/CalendarPage.vue";
 import ContactPage from "./components/pages/ContactPage.vue";
-import ContactPage from "./components/pages/GallaryPage.vue";
+import GallaryPage from "./components/pages/GallaryPage.vue";
 import VueScrollSnap from "vue-scroll-snap";
 </script>
 
